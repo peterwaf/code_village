e56eaeac51c307b43aa473432a1e76052e8b1ffe
peterwaf <peterwafulah@gmail.com>
2020-02-18 20:36:22 +0300

    more code and files

--- day_2/excercise1.py
+++ day_2/excercise1.py
@@ -1,3 +1,22 @@
 """
 This is my story
 """
+myStory = """My favourite animal is {animal} and it loves to eat alot of {food}.I have
+to import {food} from {city}"""
+
+def tellStory():
+    myFavs = dict()
+    addFav('animal',myFavs)
+    addFav('food',myFavs)
+    addFav('city',myFavs)
+    story = myStory.format(**myFavs)
+    print(story)
+
+def addFav(kavar,myFavs):
+    """This allows user input"""
+    prompt = 'Enter name of ' +kavar +":"
+    response = input(prompt).strip()
+    myFavs[kavar] = response
+
+tellStory()
+input('Press Enter to exit')
---
+++ day_2/excercise2.py
@@ -0,0 +1,35 @@
+a = 10
+b = 6
+c = 31
+sum = a + b + c
+
+def myPrinter(myVar):
+    print(myVar)
+
+myPrinter(sum)
+
+def add(x,y):
+    print(x+y)
+    
+add(2,3)
+add(5,5)
+add(10,10)
+
+def add(a,b,c):
+    print(a+b+c)
+add(2,2,3)
+add(6,2,3)
+add(10,2,3)
+
+x = int(input('Enter number x : '))
+y = int(input('Enter number y : '))
+
+def add(x,y):
+    print(x+y)
+    
+add(x,y)
+
+
+
+
+
