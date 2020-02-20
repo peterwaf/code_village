cea0c7397d430a410789b8dbe111d5f89472fab8
peterwaf <peterwafulah@gmail.com>
2020-02-20 12:44:50 +0300

    phone number,amount and balance validation

--- day_3/sendmoney.py
+++ day_3/sendmoney.py
@@ -1,12 +1,17 @@
+balance = 5000
 recipientName = input("Enter name of Recipient :")
 recipientPhoneNumber = input("Enter Recipient Phone No :")
-amount = int(input("Enter amount :"))
-
-def sendMoney(recipientName,recipientPhoneNumber,amount):
-    print('Send {} to {} of {}'.format(amount,recipientName,recipientPhoneNumber))
-    confirm = int(input('Press 1 to proceed :'))
-    if(confirm==1):
-        balance = 5000
-        newBalance = balance - amount
-        print('confirmed. KES {} sent to {} of {} balance is {}'.format(amount,recipientName,recipientPhoneNumber,newBalance))
-sendMoney(recipientName,recipientPhoneNumber,amount)
+amount = int(input("Enter the amount :"))
+if (len(str(recipientPhoneNumber))!=10):
+        print('Failed,Invalid Phone number,Please use 10 digit format 07XXXXXXXX')
+elif (amount>balance):
+        print('You have insufficient funds,Please top up your amount')
+else:
+        def sendMoney(recipientName,recipientPhoneNumber,amount):
+                print('Send {} to {} of {}'.format(amount,recipientName,recipientPhoneNumber))
+                confirm = int(input('Press 1 to proceed :'))
+                if(confirm==1):
+                        transactionAmount = amount * 0.01
+                        newBalance = balance - amount - transactionAmount
+                        print('confirmed. KES {} sent to {} of {} balance is {}, transaction fee is {}'.format(amount,recipientName,recipientPhoneNumber,newBalance,transactionAmount))
+        sendMoney(recipientName,recipientPhoneNumber,amount)
