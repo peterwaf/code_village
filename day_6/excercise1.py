0e55e6f5158377ddc12f7c14b81d5c2816b10d73
peterwaf <peterwafulah@gmail.com>
2020-02-26 13:29:12 +0300

    adding dependant functions

--- day_6/excercise1.py
+++ day_6/excercise1.py
@@ -6,7 +6,6 @@ numberOfSubjects = 0
 studentSubject = ''
 studentScore = 0
 
-
 def studentInfo(numberOfStudents,studentName,studentRegistrationNumber,studentClass,numberOfSubjects,studentSubject,studentScore):
     numberOfStudents = int(input('Enter Number of students :'))
     if numberOfStudents > 0:
@@ -31,6 +30,29 @@ def studentInfo(numberOfStudents,studentName,studentRegistrationNumber,studentCl
     print(allStudentDetails)
     
 studentInfo(numberOfStudents,studentName,studentRegistrationNumber,studentClass,numberOfSubjects,studentSubject,studentScore)                    
-                      
+"""
+use below functions with above input 
+
+def gradingSystem(marks):
+    if (marks >=80 and marks <=100):
+        return 'A'
+    elif(marks >=60 and marks<80):
+        return 'B'
+    elif(marks>40 and marks<60):
+        return 'C'
+    elif(marks<40):
+        return 'D'
+    else:
+        return 'Invalid Grade'
+
+def getMean(score,numberSubjects):
+    return score/numberSubjects
+
+def printReport(k,v):
+    the_sum = 0
+    for k,v in all_scores.items():
+        the_sum += v
+        print('{} Grade : {}'.format(k,gradingSystem(v)))
+    print('The mean Grade is {}'.format(getMean(the_sum,numberSubjects)))                  
                    
-                   
+   """                
