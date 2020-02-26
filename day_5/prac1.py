17215f00782892f216e16bca6de90742cc541ace
peterwaf <peterwafulah@gmail.com>
2020-02-26 13:16:51 +0300

    student files

--- day_5/prac1.py
+++ day_5/prac1.py
@@ -1,28 +1,32 @@
-a = 5
-b = 10
-print('{}+{}={}'.format(a,b,a+b))
+class_scores = {'0101': {'Name': 'mathew', 'Class': '1', 'scores': {'english': 45, 'maths': 98}}, '4875': {'Name': 'john', 'Class': '1', 'scores': {'geography': 56}}}
+
+def print_grades():
+    for k,v in class_scores.items():
+        for m,n in v.items():
+            print(v['scores'])
+            
+print_grades()
 
 
 
-countries = dict()
-countries['Kenya'] = 'Nairobi'
-countries['Uganda'] = 'Kampala'
+def gradingSystem(marks):
+    if (marks >=80 and marks <=100):
+        return 'A'
+    elif(marks >=60 and marks<80):
+        return 'B'
+    elif(marks>40 and marks<60):
+        return 'C'
+    elif(marks<40):
+        return 'D'
+    else:
+        return 'Invalid Grade'
 
-output = '{Kenya} {Uganda}'.format(**countries)
-print(output)
+def getMean(score,numberSubjects):
+    return score/numberSubjects
 
-"""
-Assignment
-==========================
-1 user enters multiple subjects + scores
-2 option to end
-3 output
--subject
--grade
--mean grade
-
-"""
-
-all_scores = dict()
-all_scores[subjectName] = subjectScore
-print(all_scores)
+def printReport(k,v):
+    the_sum = 0
+    for k,v in all_scores.items():
+        the_sum += v
+        print('{} Grade : {}'.format(k,gradingSystem(v)))
+    print('The mean Grade is {}'.format(getMean(the_sum,numberSubjects)))
--- day_6/excercise1.py
+++ day_6/excercise1.py
@@ -1,59 +1,36 @@
-"""Enter no of students
-for each student :
-    - capture - registration number
-    - capture Class
-    - capture Name
-    - No of subjects
-    - Enter subjects & Scores
-    -output * Grade for each student
-    output * Mean Grade for each student
-    
-* Meanscore for the class
-* Mean grade for the school
-"""
-numberOfStudents = int(input('Enter the number of students : '))
-numberOfSubjects = 0
+numberOfStudents = 0
+studentName = ''
 studentRegistrationNumber = 0
-subjectName = ''
 studentClass = ''
-students = dict()
-subjects_scores = dict()
+numberOfSubjects = 0
+studentSubject = ''
+studentScore = 0
 
 
-def studentsAdder(numberOfStudents):
+def studentInfo(numberOfStudents,studentName,studentRegistrationNumber,studentClass,numberOfSubjects,studentSubject,studentScore):
+    numberOfStudents = int(input('Enter Number of students :'))
     if numberOfStudents > 0:
-        for i in range(1,numberOfStudents+1):
-            studentName = input('Enter the student name : ')
-            studentClass = input('Enter the class : ')
-            studentRegistrationNumber = input('Enter the registration number : ')
-            
-            
-            students['class'] = studentClass
-            students['name'] = studentName
-            students['scores'] = subjects_scores
-            
-            numberOfSubjects = int(input('Enter No of subjects :'))
-            if numberOfSubjects > 0 :
-                for j in range(1,numberOfSubjects+1):
-                    subjectName = input('Enter the name of the Subject : ')
-                    studentScores = int(input('Enter the score for Subject : '))
-                    subjects_scores[subjectName] = studentScores             
-      
-    print(students)
-            
-studentsAdder(numberOfStudents)
-
-"""Enter no of students
-for each student :
-    - capture - registration number
-    - capture Class
-    - capture Name
-    - No of subjects
-    - ENter subjects & Scores
-    -output * Grade for each student
-    output * Mean Grade
+        allStudentDetails = dict()
+        for i in range(0,numberOfStudents):
+            studentDetails = dict()
+            temporaryStudentData = dict()
+            studentName = input('Student Name :')
+            studentRegistrationNumber = input('Student Registration No :')
+            studentClass = input('Enter the student class :')
+            temporaryStudentData['Name'] = studentName
+            temporaryStudentData['Class'] = studentClass
+            numberOfSubjects = int(input('Enter Number of Subjects :'))
+            if numberOfSubjects >= 0:
+                studentData = {}
+                for subjectCounter in range(0,numberOfSubjects):
+                    studentSubject = input('Enter Subject Name : ')
+                    studentScore = int(input('Enter Subject Score : '))
+                    studentData[studentSubject] = studentScore
+                    temporaryStudentData['scores'] = studentData
+            allStudentDetails[studentRegistrationNumber] = temporaryStudentData
+    print(allStudentDetails)
     
-* Meanscore for the class
-* Mean grade for the school
-
-"""
+studentInfo(numberOfStudents,studentName,studentRegistrationNumber,studentClass,numberOfSubjects,studentSubject,studentScore)                    
+                      
+                   
+                   
