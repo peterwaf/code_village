42785f7b202d0a40aa06a50959efa44cf0f04bc6
peterwaf <peterwafulah@gmail.com>
2020-02-28 14:15:51 +0300

    completing the print statement and adding the currency in the dictionary in customers dict

--- day_5/prac1.py
+++ day_5/prac1.py
@@ -1,35 +1,18 @@
-customers = {
-	54856: {
-		'First Name ': 'Peter',
-		'Last Name ': 'Wafula',
-		'Mobile No. ': 722412676,
-		'Id Number': 287856,
-		'Unique Id': 54856,
-		'Account': {
-			'Account Number': 1147896545,
-			'Account Type': 'Current',
-			'Currency': 'KES',
-			'Currency Name': 'Kenya shillings',
-			'Account Balance': 10000
-		}
-	},
-	984562: {
-		'First Name ': 'John',
-		'Last Name ': 'Kissinger',
-		'Mobile No. ': 74574548,
-		'Id Number': 28795462,
-		'Unique Id': 984562,
-		'Account': {
-			'Account Number': 14759856,
-			'Account Type': 'Savings',
-			'Currency': 'USD',
-			'Currency Name': 'Kenya shillings',
-			'Account Balance': 1000
-		}
-	}
-}
+customers = {879654: {'First Name': 'Peter', 'Last Name': 'Wafula', 'Mobile No': 745789654, 'Id Number': 289456, 'Unique Id': 879654, 'Account': {'Account Number': 1784569874, 'Account Type': 'Current', 'Account Balance': 5000, 'Currency': {'Currency Code': 'KES', 'Currency Name': 'Kenya shillings'}}}, 5547896: {'First Name': 'John', 'Last Name': 'Kissinger', 'Mobile No': 745789654, 'Id Number': 35457896, 'Unique Id': 5547896, 'Account': {'Account Number': 17457964, 'Account Type': 'Savings', 'Account Balance': 87000, 'Currency': {'Currency Code': 'USD', 'Currency Name': 'Kenya shillings'}}}}
+for x,y in customers.items():
+    print('Customer Id : {}'.format(x))
+    print('Name :',y['First Name'],y['Last Name'])
+    print('Mobile No :',y['Mobile No'])
+    print('Id Number :',y['Id Number'])
+    print('Unique Customer Id :',y['Unique Id'])
+    currency_holder = y['Account']
+    print('Account Number : ',currency_holder['Account Number'])
+    print('Account Type :',currency_holder['Account Type'])
+    print('Account Balance :',currency_holder['Account Balance'])
+    currencyCodeHolder = currency_holder['Currency']
+    print('Currency Code :',currencyCodeHolder['Currency Code'])
+    print('Currency Name :',currencyCodeHolder['Currency Name'])
 
-for a,b in customers.items():
-    print('Customer Id',a)
-    for c,d in b.items():
-        print(c,d)
+
+
+    
--- day_7/bankCustomerDetails.py
+++ day_7/bankCustomerDetails.py
@@ -3,38 +3,44 @@ starter = input('Would you like to add a new customer account? Press 1 for Yes o
 
 while (starter == '1'):
     customer = dict()
+    accountsInfo = dict()
+    accountsCurrencyDetails = dict()
     firstName = input('Enter First Name :')
     lastName  = input('Enter Last Name :')
     mobileNumber = int(input('Enter Mobile Number :'))
     idNumber = int(input('Enter Id Number :'))
     uniqueCustomerId = int(input('Customer Unique Id :'))
-    customer['First Name '] = firstName
-    customer['Last Name '] = lastName
-    customer['Mobile No. '] = mobileNumber
+    customer['First Name'] = firstName
+    customer['Last Name'] = lastName
+    customer['Mobile No'] = mobileNumber
     customer['Id Number'] = idNumber
     customer['Unique Id'] = uniqueCustomerId
-    accountsInfo = dict()
     accountsInfo['Account Number'] = int(input('Enter Account Number : '))
-    accountsInfo['Account Type'] = input('Enter Account Type 1 for Savings or 2 Current ')
+    accountsInfo['Account Type'] = input('Enter Account Type 1 for Savings or 2 Current :')
     if (accountsInfo['Account Type'] == '1'):
         accountsInfo['Account Type'] = 'Savings'
     elif(accountsInfo['Account Type'] == '2'):
         accountsInfo['Account Type'] = 'Current'
     else:
-        print('Invalid ')
-        accountsInfo['Account Type'] = input('Enter Account Type eg.Savings or Current ')
-    accountsInfo['Currency'] = input('Enter Currency Type 1 for KES or 2 for USD ')
-    if (accountsInfo['Currency'] == '1'):
-        accountsInfo['Currency'] = 'KES'
-    elif(accountsInfo['Currency'] == '2'):
-        accountsInfo['Currency'] = 'USD'
+        print('Invalid Input')
+        accountsInfo['Account Type'] = input('Enter Account Type eg.Savings or Current :')
+        
+    accountsCurrencyDetails['Currency Code'] = input('Type 1 for KES or 2 for USD :')
+    
+    if(accountsCurrencyDetails['Currency Code'] == '1'):
+        accountsCurrencyDetails['Currency Code'] = 'KES'
+    elif(accountsCurrencyDetails['Currency Code'] == '2'):
+        accountsCurrencyDetails['Currency Code'] = 'USD'
     else:
-        print('Invalid ')
-        accountsInfo['Currency'] = input('Enter Currency Type 1 for KES or 2 for USD ')
-    accountsInfo['Currency Name'] = 'Kenya shillings'
-    accountsInfo['Account Balance'] = int(input('Enter Account Balance: '))
+        print('Invalid Input')
+        accountsCurrencyDetails['Currency Code'] = input('Enter Currency Type 1 for KES or 2 for USD :')
+    
+    accountsCurrencyDetails['Currency Name'] = 'Kenya shillings'
+    accountsInfo['Account Balance'] = int(input('Enter Account Balance :'))
+    accountsInfo['Currency'] = accountsCurrencyDetails
     customer['Account'] = accountsInfo
     customers[uniqueCustomerId] = customer
+    
     """The account has the following properties:
     1.    Account Number
     2.    Account Type (current, savings)
@@ -42,7 +48,21 @@ while (starter == '1'):
     4.    Account Balance"""
     starter = input('Would you like to add another account? Press 1 for Yes or 2 for No :')
 
-for a,b in customers.items():
-    print('Customer Id',a)
-    for c,d in b.items():
-        print(c,d)
+print('Thank you !')
+
+print('====customer Data=====')
+
+for x,y in customers.items():
+    print('Customer Id : {}'.format(x))
+    print('Name :',y['First Name'],y['Last Name'])
+    print('Mobile No :',y['Mobile No'])
+    print('Id Number :',y['Id Number'])
+    print('Unique Customer Id :',y['Unique Id'])
+    currency_holder = y['Account']
+    print('Account Number : ',currency_holder['Account Number'])
+    print('Account Type :',currency_holder['Account Type'])
+    print('Account Balance :',currency_holder['Account Balance'])
+    currencyCodeHolder = currency_holder['Currency']
+    print('Currency Code :',currencyCodeHolder['Currency Code'])
+    print('Currency Name :',currencyCodeHolder['Currency Name'])
+    print('============================')
