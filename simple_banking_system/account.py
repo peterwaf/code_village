f83a4698c8123d3769a8f9c33fd6495fce02e1a0
peterwaf <peterwafulah@gmail.com>
2020-03-08 21:17:58 +0300

    creating a simple OOP banking app

---
+++ simple_banking_system/account.py
@@ -0,0 +1,9 @@
+class Account():
+    
+    def __init__(self,accountName,accountNumber,accountType,accountBalance,currency):
+        self.accountName = accountName
+        self.accountNumber = accountNumber
+        self.accountType = accountType
+        self.accountBalance = accountBalance
+        self.currency = currency
+        
---
+++ simple_banking_system/customer.py
@@ -0,0 +1,8 @@
+class Customer:
+    def __init__(self,name,idNumber,uniqueID,mobileNo,Pin,account):
+        self.name = name
+        self.idNumber = idNumber
+        self.uniqueID = uniqueID
+        self.mobileNo = mobileNo
+        self.Pin = Pin
+        self.account = account
