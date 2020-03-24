2b1f7bff676e0685f21fba011010da5d4d28440c
peterwaf <peterwafulah@gmail.com>
2020-03-24 23:36:58 +0300

    django shool project

---
+++ school_App/my_school/subjects/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from students.models import Student
+# Create your models here.
+
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+    score = models.IntegerField()
+    student = models.ForeignKey(Student,on_delete=models.CASCADE)
+    
+    class Meta:
+        db_table = 'tbl_subjects'
