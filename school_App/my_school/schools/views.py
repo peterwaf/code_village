e8f0b00bd93beb6c5800f64dfc708c859989b13a
peterwaf <peterwafulah@gmail.com>
2020-03-30 19:09:13 +0300

    setting template for schools

--- school_App/my_school/schools/views.py
+++ school_App/my_school/schools/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import School
 # Create your views here.
+
+def show_schools(request):
+    all_schools = School.objects.all()
+    context = {'all_schools':all_schools}
+    return render(request,"schools/schools.html",context)
