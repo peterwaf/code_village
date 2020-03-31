b2e9a619075bd2f1e2d0993f9ed26a930be5cf97
peterwaf <peterwafulah@gmail.com>
2020-03-31 21:30:56 +0300

    setting up inner views

--- school_App/my_school/schools/urls.py
+++ school_App/my_school/schools/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('schools/',views.show_schools,name='schools'),
+    path('schools/<int:school_id>/',views.show_students,name='school_details'),
 ]
--- school_App/my_school/schools/views.py
+++ school_App/my_school/schools/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from .models import School
+from students.models import Student
+
 # Create your views here.
 
 def show_schools(request):
     all_schools = School.objects.all()
     context = {'all_schools':all_schools}
     return render(request,"schools/schools.html",context)
+
+def show_students(request,school_id):
+    #school_detail=School.objects.get(pk=school_id) if you want to display school details
+    school_student = Student.objects.filter(school=school_id)
+    context = {'school_student':school_student}
+    return render(request,"schools/details.html",context)
+    
